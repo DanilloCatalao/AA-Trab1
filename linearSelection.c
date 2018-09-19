cf55962c1d0eb1ef610c6a45aa365c4dedb1c41f
Danillo Catalão <danillocatalao@hotmail.com>
2018-09-19 15:13:33 -0300

    tabs

--- linearSelection.c
+++ linearSelection.c
@@ -1,10 +1,11 @@
 #include"linearSelection.h"
 
 int LinearSelection( int* list, int k ){
-	//Group the numbers into sets of 5
-	//Sort individual groups and find the median of each group; put these medians in a set M
-	//Find median m’ of set M using LinearSelection(M,sizeof(M))
-	//Partition original data around m’ such that values less than it are in set L and values greater than it are in set R
-	//If sizeof(L) = k-1 return m'
-	//If sizeof(L) > k-1 retun LinearSelection()
+    //Group the numbers into sets of 5
+    //Sort individual groups and find the median of each group; put these medians in a set M
+    //Find median m’ of set M using LinearSelection(M,sizeof(M))
+    //Partition original data around m’ such that values less than it are in set L and values greater than it are in set R
+    //If sizeof(L) = k-1 return m'
+    //If sizeof(L) > k-1 retun LinearSelection()
+    return 0;
 }
--- main.c
+++ main.c
@@ -15,11 +15,11 @@ static void debug_show(int *list, int n) {
 
 
 int main(void){
-	int list[] = {2,5,9,19,24,54,5,87,9,10,44,32,18,13,2,
+    int list[] = {2,5,9,19,24,54,5,87,9,10,44,32,18,13,2,
 				4,23,26,16,17,25,39,47, 56,71};
-	printf("%d\n",LinearSelection(list,5));
+    printf("%d\n",LinearSelection(list,5));
 
     merge_sort(list, sizeof(list)/sizeof(int));
     debug_show(list, sizeof(list)/sizeof(int));
-	return;
+    return 0;
 }
