49ba6e9662e565e9e09ddf86b322d3a1908a50d6
Danillo Catalão <danillocatalao@hotmail.com>
2018-09-19 16:50:00 -0300

    Divisão dos grupos pode receber não múltiplos de 5

--- linearSelection.c
+++ linearSelection.c
@@ -5,31 +5,40 @@
 
 int LinearSelection( int* list, int n, int k ){
     //Group the numbers into sets of 5
-    //Sort individual groups and find the median of each group; put these medians in a set M
-    //Find median m’ of set M using LinearSelection(M,sizeof(M))
-    //Partition original data around m’ such that values less than it are in set L and values greater than it are in set R
-    //If sizeof(L) = k-1 return m'
-    //If sizeof(L) > k-1 retun LinearSelection()
     int group_count;
     int i;
+    int *medians;
     int **groups;
-
     group_count = n / 5;
     if(n % 5 != 0) {
 	group_count += 1;
     }
+
+    medians = (int *) malloc(group_count * sizeof(int));
 
     groups = (int **) malloc(group_count * sizeof(int*));
     if(groups == NULL) {
 	printf("Dynamic memory allocation error.\n");
 	exit(-1);
     }
-
-    for(i = 0; i < group_count; i++) {
+    //Sort individual groups and find the median of each group; put these medians in a set M
+     for(i = 0; i < group_count-1; i++) {
 	groups[i] = &list[i * 5];
 	merge_sort(groups[i], 5);
-    }
+     }
+     groups[group_count-1] = &list[(group_count-1)*5];
+     merge_sort(groups[group_count-1], n % 5);
 
+
+
+    //Find median m’ of set M using LinearSelection(M,sizeof(M))
+
+    //Partition original data around m’ such that values less than it are in set L and values greater than it are in set R
+    //If sizeof(L) = k-1 return m'
+    //If sizeof(L) > k-1 retun LinearSelection()
+    
+
+   
     free(groups);
     return 0;
 }
--- main.c
+++ main.c
@@ -17,9 +17,9 @@ static void debug_show(int *list, int n) {
 int main(void){
     int list[] = {2,5,9,19,24,54,5,87,9,10,44,32,18,13,2,
 				4,23,26,16,17,25,39,47, 56,71};
-    printf("%d\n",LinearSelection(list, sizeof(list)/sizeof(int), 5));
+    LinearSelection(list, sizeof(list)/sizeof(int), 5);
 
     /* merge_sort(list, sizeof(list)/sizeof(int)); */
-    /* debug_show(list, sizeof(list)/sizeof(int)); */
+    debug_show(list, sizeof(list)/sizeof(int)); 
     return 0;
 }
