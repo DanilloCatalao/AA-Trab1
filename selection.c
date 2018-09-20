b305813fe96ec017857e4815cde3e28505a61f2a
rodrigo.cortezi <rodrigovcortezi@gmail.com>
2018-09-20 15:03:57 -0300

    Implementação da recursão para encontrar a mediana.

--- selection.c
+++ selection.c
@@ -8,16 +8,24 @@ int LinearSelection( int* list, int n, int k ){
     int group_count;
     int medianOfMedians;
     int i, remainder;
-    int *medians;
+    int *median_list;
+    int median;
     int **groups;
-    group_count = n / 5;
 
+    // Caso base
+    if(n == 5 && k == 2) {
+	merge_sort(list, n);
+
+	return list[2];
+    }
+
+    group_count = n / 5;
     if(n % 5 != 0) {
 	group_count += 1;
     }
 
-    medians = (int *) malloc(group_count * sizeof(int));
-    if(medians == NULL) {
+    median_list = (int *) malloc(group_count * sizeof(int));
+    if(median_list == NULL) {
 	printf("Dynamic memory allocation error.\n");
 	exit(-1);
     }
@@ -32,24 +40,22 @@ int LinearSelection( int* list, int n, int k ){
     for(i = 0; i < group_count-1; i++) {
 	groups[i] = &list[i * 5];
 	merge_sort(groups[i], 5);
-	medians[i] = list[(i*5)+2];
+	median_list[i] = groups[i][2];
     }
     remainder = n - (i * 5);
     groups[group_count-1] = &list[(group_count-1)*5];
     merge_sort(groups[group_count-1], remainder);
-    medians[group_count-1] = groups[group_count-1][remainder];
+    median_list[group_count-1] = groups[group_count-1][remainder/2];
 
+    medianOfMedians = LinearSelection(median_list, 5, 2);
 
     //Find median m’ of set M using LinearSelection(M,sizeof(M))
-    //medianOfMedians = LinearSelection(medians,group_count,group_count/2);
-
-    //for(i=0;i<group_count;i++)printf(" %d ",medians[i]);
 
     //Partition original data around m’ such that values less than it are in set L and values greater than it are in set R
     //If sizeof(L) = k-1 return m'
     //If sizeof(L) > k-1 retun LinearSelection()
     
-    free(medians);
+    free(median_list);
     free(groups);
 
     return 0;
